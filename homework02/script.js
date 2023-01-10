2e4ddba1b0f892f90a3361d099dc237ea7c2505b
Arthur Procenko <arsen.osben@gmail.com>
2023-01-10 14:05:00 +0200

    hw2

--- homework02/script.js
+++ homework02/script.js
@@ -1,31 +1,27 @@
 let n, m, p, summ = 0;
-n = prompt('Input N:');
-m = prompt('Input M:');
-p = prompt('Input odd parametr', 'true');
-let validKey = true;
-let messageValid = '';
-n = parseInt(n);
-m = parseInt(m);
-if (!Number.isInteger(n)) {
-    messageValid = 'N is not integer';
-    validKey = false;
+n = +prompt('Input N:');
+while ( (!Number.isInteger(n)) || (n < 0)) {
+    alert('integer positive only');
+    n = +prompt('Input N:');
+    console.log(n);
 }
-if (!Number.isInteger(m)) {
-    messageValid = 'M is not integer';
-    validKey = false;
+
+m = +prompt('Input M:');
+while ( (!Number.isInteger(m)) || (m < 0) || (n >= m)) {
+    alert('integer positive only and > m');
+    m = +prompt('Input M:');
+    console.log(m);
 }
-if (validKey) {
-    for (let i = n; i <= m; i++) {
-        if (p == 'true') {
-            if (!((i % 2) === 0)) {
-                summ = summ + i;
-            }
-        } else {
+
+p = +confirm('Input odd parametr');
+//console.log(p);
+for (let i = n; i <= m; i++) {
+    if (p) {
+        if (!((i % 2) === 0)) {
             summ = summ + i;
         }
+    } else {
+        summ = summ + i;
     }
-    console.log('=== ', summ);
 }
-else {
-    console.log(messageValid);
-}
+console.log('=== ', summ);

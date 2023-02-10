4f10cae43894027d0a2b2cb0dbd1393e4d63ddaa
Arthur Procenko <arsen.osben@gmail.com>
2023-02-10 13:49:10 +0200

    n

--- homework15/homework15/src/index.js
+++ homework15/homework15/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import Post from './Post';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
 const ANAKIN_IMAGE = "https://cdn.oboi7.com/8f9299115b9c81f77c4df6a24aaabfe1e5c942d7/zvezdnye-vojny-dzhedaj-anakin-skajuoker-aktery-hajden-kristensen.jpg";
 const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
 

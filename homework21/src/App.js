e04e0bbb83927197a8e6b3ce3eb23e6f182d460d
Arthur Procenko <arsen.osben@gmail.com>
2023-03-01 12:18:30 +0200

    hw21

--- homework21/src/App.js
+++ homework21/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import {NoPage} from "./pages/NoPage";
 import {SingIn} from "./pages/SingIn";
 import {SingUp} from "./pages/SingUp";
@@ -6,13 +6,13 @@ import {SingUp} from "./pages/SingUp";
 export default function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<SingIn />} />
                     <Route path="/singup" element={<SingUp />} />
                     <Route path="*" element={<NoPage />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }

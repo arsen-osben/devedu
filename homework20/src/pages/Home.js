aaa3a8725127a6d6c3703321492aff01eb17ec86
Arthur Procenko <arsen.osben@gmail.com>
2023-03-01 12:03:30 +0200

    20ed

--- homework20/src/pages/Home.js
+++ homework20/src/pages/Home.js
@@ -1,10 +1,9 @@
-import routePic from './../img/route.jpg'
 
 const Home = () => {
     return (
         <div className="container">
             <h1>Hi, this is main page</h1>
-            <div className="pic"><img src={routePic} alt=""/></div>
+            <div className="pic"><img src="https://cdn.oboi7.com/8f9299115b9c81f77c4df6a24aaabfe1e5c942d7/zvezdnye-vojny-dzhedaj-anakin-skajuoker-aktery-hajden-kristensen.jpg" alt=""/></div>
         </div>
     );
 };
